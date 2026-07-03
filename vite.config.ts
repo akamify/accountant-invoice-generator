5adc26a921a7b0a8b815bfeffe91d110fa4943aa
akamify <akamifybusiness@gmail.com>
2026-07-03 15:43:18 +0530

    feat: build fixed

--- vite.config.ts
+++ vite.config.ts
@@ -5,7 +5,7 @@ import { componentTagger } from "lovable-tagger";
 
 // https://vitejs.dev/config/
 export default defineConfig(({ command, mode }) => ({
-  base: command === 'build' ? './' : '/',
+  base: '/',
   server: {
     host: "::",
     port: 5173,
